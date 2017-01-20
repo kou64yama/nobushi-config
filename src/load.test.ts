5ff067ce683ae95d72da559cec6aa0f5ac3ee368
YAMADA Koji <kou64yama@gmail.com>
2017-01-21 02:36:40 +0900

    Add tests

---
+++ src/load.test.ts
@@ -0,0 +1,25 @@
+import test from 'ava';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+process.env.NODE_ENV = 'test';
+process.env.NODE_CONFIG_DIR = join(__dirname, '../test/config');
+process.env.NODE_CONFIG_PRIVATE_KEY = readFileSync(join(__dirname, '../test/key/private.key'), 'base64');
+
+const config = require('./load');
+
+const c = {
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  datasource: {
+    url: 'postgresql://localhost:5432/apps',
+    username: 'username',
+    password: 'secret',
+  },
+};
+
+test((t) => {
+  t.deepEqual(JSON.parse(JSON.stringify(config)), c);
+});
