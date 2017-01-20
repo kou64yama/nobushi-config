b11d615eb662f32e16e5129eac423d9ff18ea1bb
YAMADA Koji <kou64yama@gmail.com>
2017-01-21 02:34:28 +0900

    Change NODE_CONFIG_PRIVATE_KEY to set Base64 string

--- src/load.ts
+++ src/load.ts
@@ -5,7 +5,8 @@ import SecureMiddleware from './middleware/SecureMiddleware';
 
 let middleware: Middleware | undefined;
 if (process.env.NODE_CONFIG_PRIVATE_KEY) {
-  middleware = new SecureMiddleware(process.env.NODE_CONFIG_PRIVATE_KEY);
+  const privateKey = new Buffer(process.env.NODE_CONFIG_PRIVATE_KEY, 'base64');
+  middleware = new SecureMiddleware(privateKey.toString('utf8'));
 }
 
 const configDir = process.env.NODE_CONFIG_DIR as string || join(process.cwd(), 'config');
