05097f68ff88b61d938150af5310317b9ae41068
YAMADA Koji <kou64yama@gmail.com>
2018-05-03 14:53:03 +0900

    Update environment

--- src/applyFilter.ts
+++
@@ -1,8 +0,0 @@
-import { Middleware, Value } from './Config';
-
-export type Filter = (value: Value, key?: string) => Value;
-
-export default (middleware: Middleware, filters: Filter[]): Middleware => ({
-  has: (map, key) => middleware.has(map, key),
-  get: (map, key) => filters.reduce((value, filter) => filter(value, key), middleware.get(map, key))
-});
