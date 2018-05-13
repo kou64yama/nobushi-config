04d4ba27283ec2bd94e625e1c026c8a32d06a41c
YAMADA Koji <kou64yama@gmail.com>
2018-05-13 14:02:27 +0900

    :art: Fix format

--- src/entries.ts
+++ src/entries.ts
@@ -3,14 +3,17 @@ import { Entry } from './ConfigSource';
 export default function entries(obj: any): Entry[] {
   return Object.keys(obj)
     .map(key => [key, obj[key]])
-    .reduce((mem, [prefix, x]) => {
-      if (typeof x !== 'object' || x === null) {
-        return [...mem, [prefix, x]];
-      }
+    .reduce(
+      (mem, [prefix, x]) => {
+        if (typeof x !== 'object' || x === null) {
+          return [...mem, [prefix, x]];
+        }
 
-      return [
-        ...mem,
-        ...entries(x).map(([key, value]) => [`${prefix}.${key}`, value]),
-      ];
-    },      []);
+        return [
+          ...mem,
+          ...entries(x).map(([key, value]) => [`${prefix}.${key}`, value]),
+        ];
+      },
+      [],
+    );
 }
